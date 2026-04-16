b12c0095e9c9e6bd09b003834d3a56d2d931bf11
Gorkem <gorkem.akdogan@sabanciuniv.edu>
2026-04-16 16:00:03 +0200

    Added is_set(c1,c2,c3) method to Computer class.

--- set_computer.py
+++ set_computer.py
@@ -4,11 +4,6 @@ class Computer:
     def __init__(self):
         pass
     def find_sets(self, table):
-        check_filling = True
-        check_color = True
-        check_shape = True
-        check_count = True
-
         set_indices = []
 
         index_c1 = 0
@@ -59,8 +54,49 @@ class Computer:
                 index_c2 += 1
             index_c1 += 1
 
-        print(set_indices)
+        return set_indices
+
+    def is_set(self, card1, card2, card3):
+        # Find filling
+        if card1.filling == card2.filling:
+            required_filling = card1.filling
+        else:
+            for filling in ["e", "s", "f"]:
+                if filling != card1.filling and filling != card2.filling:
+                    required_filling = filling
+
+        # Find color
+        if card1.color == card2.color:
+            required_color = card1.color
+        else:
+            for color in ["r", "p", "g"]:
+                if color != card1.color and color != card2.color:
+                    required_color = color
+
+        # Find shape
+        if card1.shape == card2.shape:
+            required_shape = card1.shape
+        else:
+            for shape in ["c", "d", "s"]:
+                if shape != card1.shape and shape != card2.shape:
+                    required_shape = shape
+
+        # Find count
+        if card1.count == card2.count:
+            required_count = card1.count
+        else:
+            for count in ["1", "2", "3"]:
+                if count != card1.count and count != card2.count:
+                    required_count = count
+
+        # Look if the card3 is the required card
+        if card3.filling == required_filling and card3.color == required_color and card3.shape == required_shape and card3.count == required_count:
+            return True
+        else:
+            return False
 
 computer = Computer()
 table = [image_dictionary.Card("erc1"), image_dictionary.Card("erc2"), image_dictionary.Card("erc3"), image_dictionary.Card("src1"), image_dictionary.Card("frc1")]
-computer.find_sets(table)
+print(computer.find_sets(table))
+
+print(computer.is_set(table[0], table[1], table[3]))
